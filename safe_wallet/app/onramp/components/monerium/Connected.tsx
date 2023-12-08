16565cab25c908fa319ce1eb88edecf677e4e09f
yashthipsay <yashthpsay@gmail.com>
2023-12-08 19:24:01 +0530

    onramp props

--- safe_wallet/app/onramp/components/monerium/Connected.tsx
+++ safe_wallet/app/onramp/components/monerium/Connected.tsx
@@ -1,3 +1,14 @@
 import { useState, useEffect } from 'react'
 import { AuthContext, OrderState } from '@monerium/sdk'
+
 import { Alert, Box, Button, TextField, CircularProgress as Loader } from '@mui/material'
+
+type ConnectedProps = {
+    authContext: AuthContext
+    orderState: OrderState | undefined
+    safe: string
+    onLogout: () => void
+    onTransfer: (iban: string, amount: string) => void
+  }
+
+  
