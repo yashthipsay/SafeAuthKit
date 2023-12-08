97a62c139bf920673a5c0191cdd388922582bd5a
yashthipsay <yashthpsay@gmail.com>
2023-12-08 19:33:34 +0530

    onramp api

--- safe_wallet/app/onramp/components/monerium/LoginWithMonerium.tsx
+++ safe_wallet/app/onramp/components/monerium/LoginWithMonerium.tsx
@@ -5,3 +5,28 @@ type LoginWithMoneriumProps = {
   threshold: string
   onLogin: () => void
 }
+
+function LoginWithMonerium({ safe, threshold, onLogin }: LoginWithMoneriumProps) {
+    return (
+      <>
+        <Typography variant="h5" color="primary">
+          Connected !!
+        </Typography>
+        <Typography variant="body1" sx={{ mt: 2 }}>
+          You are connected and have selected the following Safe:{' '}
+          <Typography color="primary" component="span">
+            {safe} ({threshold})
+          </Typography>
+        </Typography>
+        <Typography variant="body1" sx={{ mt: 2 }}>
+          You now can login with Monerium and link the selected Safe with your account
+        </Typography>
+        <br />
+        <Button variant="contained" onClick={onLogin}>
+          Login with Monerium
+        </Button>
+      </>
+    )
+  }
+  
+  export default LoginWithMonerium
